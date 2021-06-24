1dbd4157a10f2413214e531fe87e3bba086c1c60
zhihao.zhao2 <Arnold_zhao@126.com>
2021-06-24 15:41:52 +0800

    feat：add get test

--- es6/src/main/java/com/smart/heart/es6/example/map/GetTest.java
+++ es6/src/main/java/com/smart/heart/es6/example/map/GetTest.java
@@ -1,11 +1,17 @@
 package com.smart.heart.es6.example.map;
 
+import com.alibaba.fastjson.JSONObject;
 import com.smart.heart.es6.support.ES6Connection;
 import com.smart.heart.es6.support.template.ES6Template;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.xcontent.XContentType;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * @Description:
@@ -14,7 +20,7 @@ import java.io.IOException;
  */
 public class GetTest {
     public static void main(String[] args) throws IOException {
-        ES6Template es6Template = new ES6Template(ES6Connection.ESClientMode.REST, "10.69.0.4:9200");
+        /*ES6Template es6Template = new ES6Template(ES6Connection.ESClientMode.REST, "10.69.0.4:9200");
         GetRequest getRequest = new GetRequest("cls_lian_v1_article");
         getRequest.id("1");
         GetResponse getResponse = es6Template.getRestHighLevelClient().get(getRequest);
@@ -24,5 +30,17 @@ public class GetTest {
 
         System.out.println("test-consumer-group-arnold-1".hashCode() % 50);
         System.out.println("test-consumer-group-arnold-1".hashCode());
+        */
+
+        JSONObject log = new JSONObject();
+        log.put("A", "A");
+        IndexRequest indexRequest = new IndexRequest().index("key").type("doc").id(UUID.randomUUID().toString())
+                .source(log.toJSONString(), XContentType.JSON);
+        Map<String, Object> map1 = indexRequest.sourceAsMap();
+        map1.put("_id", indexRequest.id());
+        map1.forEach((k, v) -> {
+            System.out.println("key：" + k + ">v：" + v);
+        });
+
     }
 }
